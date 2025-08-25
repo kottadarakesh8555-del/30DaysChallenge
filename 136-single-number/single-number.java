b7536ceebc2f32c5f997cf9e5abe43fb73516191
kottadarakesh8555-del <kottadarakesh8555@gmail.com>
2025-08-25 22:20:00 +0530

    Time: 7 ms (28.67%) | Memory: 46.2 MB (60.65%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -2,12 +2,17 @@ class Solution {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
         int n=nums.length;
-        for(int i=0;i<n-1;i=i+2){
-            if(nums[i]!=nums[i+1]){
-                return nums[i];
-            }
+        // for(int i=0;i<n-1;i=i+2){
+        //     if(nums[i]!=nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[n-1];
+        int res=nums[0];
+        for(int i=1;i<n;i++){
+            res=res^nums[i];
         }
-        return nums[n-1];
+        return res;
         //We can do with set and Xor 2 different approch
     }
 }
