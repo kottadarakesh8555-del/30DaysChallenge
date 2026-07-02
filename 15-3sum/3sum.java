c27228a8822866def268ccf18c6b045eecde814a
kottadarakesh8555-del <kottadarakesh8555@gmail.com>
2026-07-02 07:55:36 +0530

    Time: 35 ms (44.04%) | Memory: 59.4 MB (32.89%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -22,6 +22,8 @@ class Solution {
                 else{
                     li.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     //li.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k && nums[j]==nums[j+1])j++;
+                    while(j<k && nums[k]==nums[k-1])k--;
                     j++;
                     k--;
                 }
